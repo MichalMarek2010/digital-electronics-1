60247cc3efe8a398e00f99de9fbbbe51c7bee421
MichalMarek2010 <99725289+MichalMarek2010@users.noreply.github.com>
2022-03-23 15:36:35 +0100

    Update cnt_up_down.vhd

--- labs/06-counter/counter/counter.srcs/sources_1/new/cnt_up_down.vhd
+++ labs/06-counter/counter/counter.srcs/sources_1/new/cnt_up_down.vhd
@@ -38,29 +38,25 @@ architecture behavioral of cnt_up_down is
     signal s_cnt_local : unsigned(g_CNT_WIDTH - 1 downto 0);
 
 begin
-    --------------------------------------------------------
-    -- p_cnt_up_down:
-    -- Clocked process with synchronous reset which implements
-    -- n-bit up/down counter.
-    --------------------------------------------------------
-    p_cnt_up_down : process(clk)
+
+p_cnt_up_down : process(clk)
     begin
         if rising_edge(clk) then
         
-            if (reset = '1') then   -- Synchronous reset
+            if (reset = '1') then               -- Synchronous reset
                 s_cnt_local <= (others => '0'); -- Clear all bits
 
-            elsif (en_i = '1') then -- Test if counter is enabled
-                if (cnt_up_i = '1') then
+            elsif (en_i = '1') then       -- Test if counter is enabled
                 -- TEST COUNTER DIRECTION HERE
+                if (cnt_up_i = '1') then
                     s_cnt_local <= s_cnt_local + 1;
-                else 
-                    s_cnt_local <= s_cnt_local -1;
+                else
+                    s_cnt_local <= s_cnt_local - 1;
                 end if;
-
             end if;
         end if;
     end process p_cnt_up_down;
+
 
     -- Output must be retyped from "unsigned" to "std_logic_vector"
     cnt_o <= std_logic_vector(s_cnt_local);
